72168b834d8d1d33215d43c1b7be6743aca73840
Saianeesh Keshav Haridas <saianeesh@protonmail.com>
2020-06-02 09:20:30 -0400

    OMF config

--- .config/fish/conf.d/omf.fish
+++ .config/fish/conf.d/omf.fish
@@ -4,4 +4,6 @@ set -q XDG_DATA_HOME
   or set -gx OMF_PATH "$HOME/.local/share/omf"
 
 # Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
+if status --is-interactive
+    source $OMF_PATH/init.fish
+end
