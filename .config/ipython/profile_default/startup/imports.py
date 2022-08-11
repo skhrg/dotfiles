855ab2a08d328ef898e66e0ec69435b523bdd594
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2022-08-11 14:00:23 +0000

    Added ipython startup scripts

---
+++ .config/ipython/profile_default/startup/imports.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal as sig
+import scipy.optimize as opt
+import datetime as dt
+import os
+import sys
+from importlib import reload
