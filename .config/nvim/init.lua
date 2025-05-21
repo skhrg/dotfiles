41e521db282d18af53e3c8981855378ecf21680f
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2025-05-21 11:38:53 -0400

    updates to coq

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -30,14 +30,39 @@ require("lazy").setup({
     { "williamboman/mason.nvim"},
     { "neovim/nvim-lspconfig"},
     { "williamboman/mason-lspconfig.nvim"},
-    { "ms-jpq/coq_nvim",  branch = "coq",
-        init = function() vim.g.coq_settings = { auto_start = true } end,
-        lazy = False},
-    { "ms-jpq/coq.artifacts"},
     { "ray-x/lsp_signature.nvim"},
     { "jbyuki/nabla.nvim"},
     { "nacro90/numb.nvim"},
     {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    {
+  "neovim/nvim-lspconfig", -- REQUIRED: for native Neovim LSP integration
+  lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
+  dependencies = {
+    -- main one
+    { "ms-jpq/coq_nvim", branch = "coq" },
+
+    -- 9000+ Snippets
+    { "ms-jpq/coq.artifacts", branch = "artifacts" },
+
+    -- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
+    -- Need to **configure separately**
+    { 'ms-jpq/coq.thirdparty', branch = "3p" }
+    -- - shell repl
+    -- - nvim lua api
+    -- - scientific calculator
+    -- - comment banner
+    -- - etc
+  },
+  init = function()
+    vim.g.coq_settings = {
+        auto_start = true, -- if you want to start COQ at startup
+        -- Your COQ settings here
+    }
+  end,
+  config = function()
+    -- Your LSP settings here
+  end,
+}
 })
 -- To add down the line:
 -- vimtex
